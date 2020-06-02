1e58325f5924809c0946994dd316f1003a07a8e2
Carlo Jeske <jeske@sc-networks.com>
2020-06-02 13:25:04 +0200

    [push] Added push to repository functionality

---
+++ src/util/forEach.ts
@@ -0,0 +1,14 @@
+export function mapObject<T, K>(
+  obj: T,
+  callback: (propertyValue: T[keyof T], propertyName: keyof T) => K
+) {
+  const result: Partial<{ [key in keyof T]: K }> = {};
+  Object.entries(obj).forEach(([propertyName, propertyValue]) => {
+    result[propertyName as keyof T] = callback(
+      propertyValue,
+      propertyName as keyof T
+    );
+  });
+
+  return result as { [key in keyof T]: K };
+}
