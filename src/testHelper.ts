7dc3732922fdf712b9bd224649eb17679ea9727a
plusgut <github@webentwickler2-0.de>
2020-12-08 15:06:22 +0100

    [typescript] upgrade

--- src/testHelper.ts
+++ src/testHelper.ts
@@ -22,6 +22,6 @@ export function promiseHandler<T, U>(cb: (data: T) => U) {
 
 export async function tick(count: number) {
   for (let i = 0; i < count; i += 1) {
-    await new Promise((resolve) => resolve());
+    await new Promise<void>((resolve) => resolve());
   }
 }
